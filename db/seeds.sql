39702ff329a92a3d9dd1fd830ec9a25ce59023d3
SandersonHub <128574459+SandersonHub@users.noreply.github.com>
2023-07-26 21:20:38 -0400

    add some info to seed.sql along with some place holders

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,23 @@
+--query putting data into department table
+INSERT INTO department (name)
+--values data being put into the table (into a column)
+values  ("Engineering"),
+        ("Finance"),
+        ("Interconnected"),
+        ("Sales");
 
+--data table
+
+INSERT INTO role (title, salary, department_id)
+values
+
+--data table
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+values
+
+--data table
 
 
 --place holders
